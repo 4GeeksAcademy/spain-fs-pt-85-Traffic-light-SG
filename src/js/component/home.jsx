0ad8aaaa186566f380b2594d2423e737266b78a9
Sergi-03 <robrovskyyt@gmail.com>
2024-11-20 20:06:47 +0000

    finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [selectedColor, setSelectedColor] = useState("");
+	const clickCondition = (color) =>{
+		setSelectedColor(selectedColor === color ? "" : color);
+	}
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="traffic-light" style={{background: "black", display: "inline-block",
+		padding: "15px", borderRadius: "15px", position: "relative", left:"580px",
+		top: "150px"}}>
+			<div className="red" onClick={() => clickCondition("red")} style={{borderRadius: "50px", 
+			width: "100px", height: "100px",
+			background: "red", 
+			boxShadow: selectedColor === "red"? "0px 0px 9px 4px #FFD700" : "none"
+			}}></div>
+			<div className="yellow" onClick={() => clickCondition("yellow")} style={{borderRadius: "50px", 
+			width: "100px", height: "100px",
+			background: "yellow", 
+			boxShadow: selectedColor === "yellow"? "0px 0px 9px 4px #FFD700" : "none"}}></div>
+			<div className="green" onClick={() => clickCondition("green")} style={{borderRadius: "50px", 
+			width: "100px", height: "100px",
+			background: "green", 
+			boxShadow: selectedColor === "green"? "0px 0px 9px 4px #FFD700" : "none"}}></div>
 		</div>
 	);
 };
